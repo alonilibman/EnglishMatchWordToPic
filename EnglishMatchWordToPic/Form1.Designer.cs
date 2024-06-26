3f1cd8cde49e0aa9ad06c996c7b848a3f8d4e828
noname <noname@DESKTOP-0PFNJ8F>
2024-06-26 17:44:10 +0300

    Add project files.

---
+++ EnglishMatchWordToPic/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace EnglishMatchWordToPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(471, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(580, 405);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(471, 623);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(786, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 114);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(471, 757);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 114);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(786, 757);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(264, 114);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.BackColor = System.Drawing.Color.Transparent;
+            this.lblQuestion.Font = new System.Drawing.Font("Franklin Gothic Medium", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.ForeColor = System.Drawing.Color.White;
+            this.lblQuestion.Location = new System.Drawing.Point(471, 527);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblQuestion.Size = new System.Drawing.Size(579, 69);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "אֵיזוֹ חַיָּה מוֹפִיעָה בַּתְּמוּנָה?";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.MouseLeave += new System.EventHandler(this.hover_colors);
+            this.lblQuestion.MouseHover += new System.EventHandler(this.hover_colors);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(471, 22);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(579, 69);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "חִידוֹן חַיּוֹת בְּשָׂפָה הָאַנְגְּלִית";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1532, 930);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "חִידוֹן חַיּוֹת בְּשָׂפָה הָאַנְגְּלִית";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EnglishMatchWordToPic/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnglishMatchWordToPic
+{
+    public partial class Form1 : Form
+    {
+        // quiz game variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int totalQuestions;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            askQuestion(questionNumber);
+            totalQuestions = 15;
+        }
+        private void hover_colors(object sender, EventArgs e)
+        {
+            if (lblQuestion.ForeColor == Color.White)
+            {
+                lblQuestion.ForeColor = Color.Black;
+            }
+            else
+            {
+                lblQuestion.ForeColor = Color.White;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }  
+
+   
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if ((buttonTag==correctAnswer))
+            {
+                score++;
+            }
+            if (questionNumber == totalQuestions)
+            {
+                MessageBox.Show(" כָּל הַכָּבוֹד ! הִרְוַחְתָּ " + (score) + " נְקֻדּוֹת"
+                    +Environment.NewLine+
+                    "?נְשַׂחֵק שׁוּב"
+                    );
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+
+
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+        private void askQuestion(int qnum)
+        {
+            switch (qnum) // question number
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.bat;
+                    button1.Text = "Dog";
+                    button2.Text = "Fish";
+                    button3.Text = "Shark";
+                    button4.Text = "Bat";
+                    correctAnswer = 4;
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.bee;
+                    button1.Text = "Frog";
+                    button2.Text = "Koala";
+                    button3.Text = "Bee";
+                    button4.Text = "Tiger";
+                    correctAnswer = 3;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.bird;
+                    button1.Text = "Bird";
+                    button2.Text = "Monkey";
+                    button3.Text = "Cat";
+                    button4.Text = "Dolphin";
+                    correctAnswer = 1;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.cat;
+                    button1.Text = "Owl";
+                    button2.Text = "Zebra";
+                    button3.Text = "Shark";
+                    button4.Text = "Cat";
+                    correctAnswer = 4;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.crab;
+                    button1.Text = "Penguin";
+                    button2.Text = "Crab";
+                    button3.Text = "Bird";
+                    button4.Text = "Lion";
+                    correctAnswer = 2;
+
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.dog;
+                    button1.Text = "Dog";
+                    button2.Text = "Giraffe";
+                    button3.Text = "Lizard";
+                    button4.Text = "Frog";
+                    correctAnswer = 1;
+
+                    break;
+
+                case 7:
+                    pictureBox1.Image = Properties.Resources.duck;
+                    button1.Text = "Cat";
+                    button2.Text = "Snake";
+                    button3.Text = "Duck";
+                    button4.Text = "Bat";
+                    correctAnswer = 3;
+
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources.fish;
+                    button1.Text = "Crocodile";
+                    button2.Text = "Fish";
+                    button3.Text = "Shark";
+                    button4.Text = "Cheetah";
+                    correctAnswer = 2;
+
+                    break;
+
+                case 9:
+                    pictureBox1.Image = Properties.Resources.frog;
+                    button1.Text = "Elephant";
+                    button2.Text = "Whale";
+                    button3.Text = "Ant";
+                    button4.Text = "Frog";
+                    correctAnswer = 4;
+
+                    break;
+
+                case 10:
+                    pictureBox1.Image = Properties.Resources.horse;
+                    button1.Text = "Horse";
+                    button2.Text = "Rhino";
+                    button3.Text = "Bee";
+                    button4.Text = "Panther";
+                    correctAnswer = 1;
+
+                    break;
+
+                case 11:
+                    pictureBox1.Image = Properties.Resources.mouse;
+                    button1.Text = "Mouse";
+                    button2.Text = "Pig";
+                    button3.Text = "Lizard";
+                    button4.Text = "Dog";
+                    correctAnswer = 1;
+
+                    break;
+
+                case 12:
+                    pictureBox1.Image = Properties.Resources.panda;
+                    button1.Text = "Kangaroo";
+                    button2.Text = "Octopus";
+                    button3.Text = "Panda";
+                    button4.Text = "Bat";
+                    correctAnswer = 3;
+
+                    break;
+
+                case 13:
+                    pictureBox1.Image = Properties.Resources.snake;
+                    button1.Text = "Leopard";
+                    button2.Text = "Snake";
+                    button3.Text = "Whale";
+                    button4.Text = "Deer";
+                    correctAnswer = 2;
+
+                    break;
+
+                case 14:
+                    pictureBox1.Image = Properties.Resources.tiger;
+                    button1.Text = "Koala";
+                    button2.Text = "Fish";
+                    button3.Text = "Gorilla";
+                    button4.Text = "Tiger";
+                    correctAnswer = 4;
+
+                    break;
+
+                case 15:
+                    pictureBox1.Image = Properties.Resources.zebra;
+                    button1.Text = "Zebra";
+                    button2.Text = "Bear";
+                    button3.Text = "Parrot";
+                    button4.Text = "Monkey";
+                    correctAnswer = 1;
+
+                    break;
+            }
+        }
+
+        
+    }
+}
